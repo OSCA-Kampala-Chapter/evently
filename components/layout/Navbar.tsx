99131548aa236dcfca02643d69374f51b72eaf53
HassanBahati <mukisabahati@gmail.com>
2023-03-17 13:57:58 +0300

    feat: add action path to nav button

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,15 +1,20 @@
 import { GitHubIcon } from "@/lib/icons";
-import { LOGO, LOGO_ALT } from "@/lib/utils";
+import { GITHUB_LINK, LOGO, LOGO_ALT } from "@/lib/utils";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
   return (
     <nav className="sticky top-0 z-10 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Image src={LOGO} alt={LOGO_ALT} height={150} width={150} />
           <div className="flex space-x-4 text-gray-900">
-            <button className="bg-white text-black px-4 py-1 border rounded-lg flex space-x-2">
+            <button
+              className="bg-white text-black px-4 py-1 border rounded-lg flex space-x-2"
+              onClick={() => router.push(GITHUB_LINK)}
+            >
               <GitHubIcon /> <span>Star on Github</span>
             </button>
           </div>
