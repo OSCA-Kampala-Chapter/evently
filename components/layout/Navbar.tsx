d903e051b5a5b144f81e2f8d4b6c9d3ed6937461
HassanBahati <mukisabahati@gmail.com>
2023-03-17 13:12:03 +0300

    chore: adjust logo size

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="sticky top-0 z-10 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <Image src={LOGO} alt={LOGO_ALT} height={50} width={50} />
+          <Image src={LOGO} alt={LOGO_ALT} height={150} width={150} />
           <div className="flex space-x-4 text-gray-900">
             <button className="bg-white text-black px-6 py-1 border rounded-lg">
               Star on Github
