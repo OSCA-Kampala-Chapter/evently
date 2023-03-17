ad1e800079ba2cad24f279f8feaba042652921d5
HassanBahati <mukisabahati@gmail.com>
2023-03-17 12:58:19 +0300

    chore: add user-images.githubusercontent.com to next.config.js

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { LOGO, LOGO_ALT } from "@/lib/utils";
 import Image from "next/image";
 
@@ -6,19 +8,14 @@ export default function Navbar() {
     <nav className="sticky top-0 z-10 bg-black backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <div className="flex">
-            <Image src={LOGO} alt={LOGO_ALT} height={50} width={50}/>
-            <span className="text-2xl text-gray-900 font-semibold">
-              evently
-            </span>
-          </div>
+          <Image src={LOGO} alt={LOGO_ALT} height={50} width={50} />
           <div className="flex space-x-4 text-gray-900">
             <button className="bg-white text-black px-6 py-1 border rounded-lg">
               Star on Github
             </button>
           </div>
         </div>
-      </div>
+      </div>  
     </nav>
   );
 }
