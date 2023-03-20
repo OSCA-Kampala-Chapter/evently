b8dd832c5eccd1f2e69e03f0c80952eaab297936
HassanBahati <mukisabahati@gmail.com>
2023-03-21 00:56:55 +0300

    chore: add home page hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 export default function Home() {
   return (
     <main>
-      <section className="flex flex-col items-center text-white">
-        <h1 className="text-4xl  font-bold underline">
-          Dont miss any tech event, hackathon, conference
-        </h1>
-        <p>
-          Find and Discover tech events, hackathons, and conferences happening
-          around Uganda
-        </p>
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
+            <h1 className="text-3xl font-extrabold sm:text-5xl">
+              <strong className="font-extrabold text-yellow-500">
+                Dont miss any tech event, hackathon, conference
+              </strong>{' '}
+            </h1>
+
+            <p className="mt-4 sm:text-xl text-white sm:leading-relaxed">
+              Find and Discover tech events, hackathons, and conferences
+              happening around Uganda
+            </p>
+          </div>
+        </div>
       </section>
     </main>
   )
