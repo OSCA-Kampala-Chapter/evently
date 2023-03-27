11414ff28cb3ee165009c9db261d55da0f8ca544
HassanBahati <mukisabahati@gmail.com>
2023-03-27 16:02:43 +0300

    feat: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,29 @@ export default function Home() {
       </div>
     )
   }
+  function SearchBar() {
+    return (
+      <form>
+        <div className="flex">
+          <label
+            htmlFor="search-dropdown"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Your Email
+          </label>
+
+          <div className="relative w-full">
+            <input
+              type="search"
+              id="search-dropdown"
+              className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+              placeholder="Search Hackathons, Tech Events..."
+            />
+          </div>
+        </div>
+      </form>
+    )
+  }
   return (
     <main>
       <section>
@@ -46,6 +69,7 @@ export default function Home() {
               Find and Discover tech events, hackathons, and conferences
               happening around Uganda
             </p>
+            <SearchBar />
           </div>
         </div>
       </section>
