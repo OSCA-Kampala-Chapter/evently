1b5fbef94b110c964025b05858906c95151b7fdc
HassanBahati <mukisabahati@gmail.com>
2023-03-27 15:51:47 +0300

    feat: create card components and render on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,36 @@
+import Image from 'next/image'
+
 export default function Home() {
+  function Card() {
+    return (
+      <div className="max-w-xs rounded overflow-hidden shadow-lg bg-white">
+        <Image
+          className="w-full"
+          src="https://images.unsplash.com/photo-1638029202288-451a89e0d55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          alt="Sunset in the mountains"
+          height={500}
+          width={500}
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">evently hackathon</div>
+          <p className="text-gray-700 text-base">
+            Lets hack events and tech parties...
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #js
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #python
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #typescript
+          </span>
+        </div>
+      </div>
+    )
+  }
   return (
     <main>
       <section>
@@ -16,6 +48,10 @@ export default function Home() {
             </p>
           </div>
         </div>
+      </section>
+      <section className="grid lg:grid-cols-3 md:grid-cols-2 gap-y-8 place-items-center gap-x-1 lg:mx-20 md:mx-8 mx:2 grid-cols-1">
+        <Card />
+        <Card /> <Card /> <Card /> <Card /> <Card /> <Card />
       </section>
     </main>
   )
