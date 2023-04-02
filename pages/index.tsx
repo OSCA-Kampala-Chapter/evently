fbf02d7c61b93e4d054e6a249d89ac6c7f956888
HassanBahati <mukisabahati@gmail.com>
2023-04-02 20:07:50 +0300

    fix: fix undefined loop

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
           </p>
         </div>
         <div className="px-6 pt-4 pb-2">
-        {Events.tags.map((tag: string, index: number) => (
+        {Events.tags?.map((tag: string, index: number) => (
           <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             {tag}
           </span>
