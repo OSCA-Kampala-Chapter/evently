e1ff66ae2b3557c7c59d02c7edb9ac5b21e35b40
HassanBahati <mukisabahati@gmail.com>
2023-04-16 13:37:16 +0300

    fix: event rendering - create events card component - create search component - create types file for shared types

---
+++ components/Search.tsx
@@ -0,0 +1,23 @@
+export default function SearchBar() {
+  return (
+    <form>
+      <div className="flex">
+        <label
+          htmlFor="search-dropdown"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Your Email
+        </label>
+
+        <div className="relative w-full">
+          <input
+            type="search"
+            id="search-dropdown"
+            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+            placeholder="Search Hackathons, Tech events..."
+          />
+        </div>
+      </div>
+    </form>
+  )
+}
